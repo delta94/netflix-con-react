13b8a0ff454b7e1fde297247564383b7858f8caa
Jordi Gomper <jordigomper@gmail.com>
2017-10-22 16:51:59 +0200

    Creo el archivo index.js y App.js. index.js llama a la aplicaci                                                                                                    ón que se encuentra en App.js. He creado los componentes necesarios. Solo los co                                                                                                    mponentes App, TitleList y ListToggle contienen lógica

---
+++ App.js
@@ -0,0 +1,99 @@
+import React from 'react';
+import { Component } from 'react'
+
+export default class App extends Component {
+	constructor(props) {
+		super(props);
+
+		this.state = {
+
+		}
+	}
+
+	render() {
+		return(
+
+		)
+	}
+}
+
+class Navigation extends Component {
+	render() {
+		return(
+
+		)
+	}
+}
+
+class UserProfile extends Component {
+	render() {
+		return(
+
+		)
+	}
+}
+
+class Hero extends Component {
+	render() {
+		return(
+
+		)
+	}
+}
+
+class HeroButton extends Component {
+	render() {
+		return(
+
+		)
+	}
+}
+
+class TitleList extends Component {
+	constructor(props) {
+		super(props);
+
+		this.state = {
+
+		}
+	}
+
+	render() {
+		return(
+
+		)
+	}
+}
+
+class Item extends Component {
+	render() {
+		return(
+
+		)
+	}
+}
+
+class ListToggle extends Component {
+	constructor(props) {
+		super(props);
+
+		this.state = {
+
+		}
+	}
+
+	render() {
+		return(
+
+		)
+	}
+}
+
+class Logo extends Component {
+	render() {
+		return(
+
+		)
+	}
+}
+
