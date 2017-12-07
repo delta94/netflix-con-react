83b2e2b50a61155da9a6ec4ec4493c710b414813
Jordi Gomper <jordigomper@gmail.com>
2017-12-07 20:02:28 +0100

    nuevo refactor jsmentoring

--- index.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from "./containers/App.js"
-import "./css/Index.css"
-
-ReactDOM.render(
-	<App />,
-	document.getElementById('root')
-);
